fe4a5dbdc2b61b52ed1c1117942911e5e2e71440
Dzhanet <jannarustamova20@gmail.com>
2023-02-09 16:19:27 +0300

    test commit

--- main/main.py
+++ main/main.py
@@ -6,4 +6,6 @@ main = Blueprint('main', __name__, template_folder="templates")
 @main.route("/")
 def page_index():
     """возвращает форму поиска по постам"""
+
+    print('Hello')
     return render_template("/main/index.html")
